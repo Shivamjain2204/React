4edb9a1aa6bd46a723ea3cfbad409fd8811558f0
Shivam <jainshivam2204@gmail.com>
2023-08-30 20:41:55 +0530

    bgChanger Completed

--- src/MyComponents/bgChanger/BgChanger.js
+++ src/MyComponents/bgChanger/BgChanger.js
@@ -6,12 +6,19 @@ const BgChanger = () => {
   return (
     <div className='background' style={{backgroundColor: color}} >
       <div className='bar'>
-        <div className='barinner'>
-
+        <div className='bar-inner'>
+          <button onClick={()=> setColor("red")} className='button' style={{backgroundColor: "red"}} >Red</button>
+          <button onClick={()=> setColor("black")} className='button' style={{backgroundColor: "black"}} >Black</button>
+          <button onClick={()=> setColor("green")} className='button' style={{backgroundColor: "green"}} >Green</button>
+          <button onClick={()=> setColor("aqua")} className='button' style={{backgroundColor: "aqua"}} >Aqua</button>
+          <button onClick={()=> setColor("pink")} className='button' style={{backgroundColor: "pink"}} >Pink</button>
+          <button onClick={()=> setColor("blue")} className='button' style={{backgroundColor: "blue"}} >Blue</button>
+          <button onClick={()=> setColor("yellow")} className='button' style={{backgroundColor: "yellow"}} >Yellow</button>
+          <button onClick={()=> setColor("gray")} className='button' style={{backgroundColor: "gray"}} >Gray</button>
         </div>
       </div>
-    </div> 
-  )
+    </div>  
+  ) 
 }
 
 export default BgChanger
